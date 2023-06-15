f983cf4ea9228aca3572a4c76fa21d4f30565c27
Plohn <giannis.pl13@gmail.com>
2023-06-15 21:56:53 +0300

    Initial Commit

---
+++ Lempel-78/decoding.py
@@ -0,0 +1,10 @@
+# Example symbols ["A","B","C","A","D","A"]
+#
+characters = {
+#    <Index>:[<IndexPointer>,<Character>]
+    1:[0,"A"],
+    2:[0,"B"],
+    3:[0,"C"],
+    4:[1,"D"],
+    5:[1,""],
+}
