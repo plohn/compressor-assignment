4ae3e632ef3c6f2c1b821997b3782514d659d744
Plohn <giannis.pl13@gmail.com>
2023-06-19 22:24:23 +0300

    Create copressed file and write only references and characters

--- Lempel-78/compress.py
+++ Lempel-78/compress.py
@@ -1,8 +1,9 @@
 def compress(phrase: str) -> dict:
-    dictonary:dict  = dict()
+    dictionary:dict  = dict()
     encoded:dict    = dict()
     entry:str       = str()
     index:int       = 1
+    phrase          = str(phrase)
     phrase         += "\0" #Add endline character
     
     for char in phrase:
@@ -10,7 +11,7 @@ def compress(phrase: str) -> dict:
         if list(
             filter( 
                 lambda index: 
-                    index[1] == entry, dictonary.items() 
+                    index[1] == entry, dictionary.items() 
             )): continue
         else:
             if (len(entry) > 1) :
@@ -19,16 +20,31 @@ def compress(phrase: str) -> dict:
                 #index of the value is the first element 0 of the returned array.
                 refence = list(filter(
                     lambda index:
-                        index[1] == entry[:-1], dictonary.items()
+                        index[1] == entry[:-1], dictionary.items()
                 ))[0][0]
                 encoded[index] = [refence,entry[-1:]]
             else:
                 encoded[index] = [0,entry]
-            dictonary[index] = entry
+            dictionary[index] = entry
             entry = str()
         index+=1
     return encoded
 
-print(compress("ABCC"))
+def createFile(encodedmsg: list):
+    data: str() = str()
+    for value in encodedmsg:
+        for item in value:
+            data+=str(item)
+    with open("output.cmp","wb") as f:
+        f.write(bytes(data, 'utf-8'))
+    return data
 
+with open("sample.txt","rb") as input:
+    createFile(
+        list(
+            compress(input.read()).values()
+        )
+    )
 
+    
+    
