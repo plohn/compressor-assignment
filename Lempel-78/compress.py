8aed3dadabc5d4142add1434c0e242d8aa1f0de7
Plohn <giannis.pl13@gmail.com>
2023-06-28 23:06:05 +0300

    Small fixes , added last byte in file argument.

--- Lempel-78/compress.py
+++ Lempel-78/compress.py
@@ -3,13 +3,13 @@ def compress(file: bytearray) -> dict:
     encoded:dict    = dict()
     entry:bytearray = bytearray()
     index:int       = 1
-   
+    file.append(1)
     for byte in file:
         entry.append(byte)
         if list(
             filter( 
                 lambda index: 
-                    index[1] == entry, dictionary.items() 
+                    index[1] == bytes(entry), dictionary.items() 
             )): continue
         else:
             if (len(entry) > 1) :
@@ -18,12 +18,12 @@ def compress(file: bytearray) -> dict:
                 #index of the value is the first element 0 of the returned array.
                 reference = list(filter(
                     lambda index:
-                        index[1] == entry[:-1], dictionary.items()
+                        index[1] == bytes(entry[:-1]), dictionary.items()
                 ))[0][0]
-                encoded[index] = [reference,entry[-1:]]
+                encoded[index] = [reference,bytes(entry[-1:])]
             else:
-                encoded[index] = [0,entry]
-            dictionary[index] = entry
+                encoded[index] = [0,bytes(entry)]
+            dictionary[index] = bytes(entry)
             entry = bytearray()
         index+=1
     return encoded
@@ -41,12 +41,7 @@ compressedFile:bytearray = bytearray()
 
 with open("test.txt","rb") as input:
     entry:bytearray = bytearray(input.read())
-    for ref in list(compress(entry).values())
-        for symbol in ref:
-            if (isinstance(symbol, int)):
-                compressedFile.append(symbol)
-            else:
-                compressedFile.extend(symbol)
+    print(list(compress(entry).values()))
 
 with open("output","wb") as f:
     f.write(compressedFile)
